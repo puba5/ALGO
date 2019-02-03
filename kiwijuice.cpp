5904afe9538de04663700d0703db6c17c4d02ed3
puba5 <puba5@naver.com>
2019-02-03 19:52:53 +0900

    example changed

--- kiwijuice.cpp
+++ kiwijuice.cpp
@@ -4,22 +4,22 @@
 
 using namespace std;
 
-class KiwiJuiceEasy{
+class KiwiJuiceEasy {
 
-    public:
-    vector<int> thePouring(vector<int> capacities, vector<int> bottles, vector<int> fromId,
-    vector<int> toId){
-        for(int i=0;i<toId.size();i++){
-            int space = capacities[i]- bottles[i];
+public:
+    vector<int> thePouring(vector<int> capacities, vector<int> bottles, vector<int> fromId, vector<int> toId) {
+        for (int i = 0; i < toId.size(); i++) {
 
-            if(bottles[fromId[i]]>space){
-                bottles[toId[i]]=capacities[toId[i]];
-                bottles[fromId[i]]-=space;
+            int space = capacities[toId[i]] - bottles[toId[i]];
 
-            }else{
-                bottles[toId[i]] += bottles[fromId[i]];
+            if (bottles[fromId[i]] > space) {
+                bottles[toId[i]] = capacities[toId[i]];
                 bottles[fromId[i]] -= space;
-        
+
+            } else {
+                bottles[toId[i]] += bottles[fromId[i]];
+                bottles[fromId[i]] = 0;
+
             }
 
 
@@ -31,12 +31,14 @@ class KiwiJuiceEasy{
 };
 
 
-int main(){
-KiwiJuiceEasy a;
-vector<int> b = {20,20};
-vector<int> c = {5,8};
-vector<int> d = {0};
-vector<int> e = {1};
-cout << a.thePouring(b,c,d,e)[0];
+int main() {
+    KiwiJuiceEasy a;
+    vector<int> b = {700000, 800000, 900000, 1000000};
+    vector<int> c = {6,34 , 27,38,9,60};
+    vector<int> d = {1,2,4,5,3,3,1,0};
+    vector<int> e = {0,1,2,4,2,5,3,1};
+    for(int i=0;i<b.size();i++)
+    cout << a.thePouring(b, c, d, e)[i] << " ";
+
     return 0;
 }
